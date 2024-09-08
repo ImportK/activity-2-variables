6f2d3c81e0f7f9aacb47ab8ecdc029aecc3a0390
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-09-08 22:11:41 +0800

    Initial commit with variable.js

---
+++ variable.js
@@ -0,0 +1,11 @@
+//  Storing data in variables.
+let name = "Kaye Janine M. Remigio"
+let whatDoYouWannaBecomeInYourLife = "programmer"
+let gender = "female"
+let twitterHandle = "@kayEthereal"
+
+// Logging all the variables to the console.
+console.log("Name: " + name)
+console.log("What do you wanna become in your life? " + whatDoYouWannaBecomeInYourLife)
+console.log("Gender: " + gender)
+console.log("twitterHandle: " + twitterHandle)
